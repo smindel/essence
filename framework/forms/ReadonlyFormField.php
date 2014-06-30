fe07c9081c3a7fc5c9f8449f7b5f73a677d89055
Andreas Piening <piening@3online.de>
2014-07-01 00:08:23 +0200

    readonlyfield

---
+++ framework/forms/ReadonlyFormField.php
@@ -0,0 +1,18 @@
+<?php
+
+class ReadonlyFormField extends FormField
+{
+    public static function create()
+    {
+        $args = func_get_args();
+        $name = array_shift($args);
+        $label = count($args) ? array_shift($args) : $name;
+        $value = count($args) ? array_shift($args) : null;
+        return parent::create(compact('name', 'label', 'value'));
+    }
+
+    public function __toString()
+    {
+        return '<div class="field ' . get_class($this) . '"><div>' . $this->getError() . "</div><label for=\"{$this->name}\">{$this->label}</label><span id=\"{$this->name}\">{$this->value}</span></div>";
+    }
+}
