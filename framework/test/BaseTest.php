e53b791eb915f922e4f7b01b115232adc87a0218
Andreas Piening <piening@3online.de>
2014-07-15 16:19:32 +0200

    add an actual test

--- framework/test/BaseTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class BaseTest extends PHPUnit_Framework_TestCase
-{
-    public function testPushAndPop()
-    {
-        $this->assertTrue(true);
-    }
-}
---
+++ framework/test/ControllerTest.php
@@ -0,0 +1,51 @@
+<?php
+
+class ControllerTest extends PHPUnit_Framework_TestCase
+{
+    public function testRequestHandling()
+    {
+        $controllerclass = 'ControllerTest_Controller';
+        $methodname = 'lowercase';
+        $param = 'Param';
+
+        $request = Request::create(implode('/', array($controllerclass, $methodname, $param)));
+        $controller = Base::create($request->consume());
+        $response = $controller->handleRequest($request);
+
+        $this->assertEquals(strtolower($param), $response);
+    }
+
+    public function testNestedRequestHandling()
+    {
+        $controllerclass = 'ControllerTest_Controller';
+        $methodname = 'forward';
+        $param[] = 'ControllerTest_Controller';
+        $param[] = 'uppercase';
+        $param[] = 'Test';
+
+        $request = Request::create(implode('/', array($controllerclass, $methodname, implode('/', $param))));
+        $controller = Base::create($request->consume());
+        $response = $controller->handleRequest($request);
+
+        $this->assertEquals(strtoupper(array_pop($param)), $response);
+    }
+}
+
+class ControllerTest_Controller extends Controller
+{
+    function lowercase_action($param)
+    {
+        return strtolower($param);
+    }
+
+    function uppercase_action($param)
+    {
+        return strtoupper($param);
+    }
+
+    function forward_action($controllerclass)
+    {
+        $subcontroller = $controllerclass::create($this);
+        return $subcontroller->handleRequest($this->getRequest());
+    }
+}
