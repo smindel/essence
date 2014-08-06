87ff212b35390e1985b4aae597f44c71400cf0e2
Andreas Piening <piening@3online.de>
2014-08-06 18:41:20 +0200

    do some js magic on hasoneformfield

---
+++ framework/resources/HasOneFormField.js
@@ -0,0 +1,32 @@
+window.onload = function() {
+
+    var fields = document.getElementsByClassName('HasOneFormFieldOptions');
+
+    var onafterselect = function(event) {
+        var target = event.target;
+        var value = target.value;
+        var listid = target.getAttribute('list');
+        var list = document.getElementById(listid);
+        var options = list.children;
+        var label = null;
+        var datafieldname = target.getAttribute('data-data-field-name');
+        var datafield = document.getElementsByName(datafieldname)[0];
+        var editlinkid = target.getAttribute('data-edit-link-id');
+        var editlink = document.getElementById(editlinkid);
+        var editurl = null;
+        for (var i = 0; i < options.length; i++) if (options[i].value == value) {
+            label = options[i].textContent;
+            editurl = options[i].getAttribute('data-edit-url');
+        }
+
+        datafield.setAttribute('value', value);
+        datafield.value = value;
+        target.setAttribute('placeholder', label);
+        target.value = '';
+        editlink.setAttribute('href', editurl);
+    }
+
+    for(var i = 0; i < fields.length; i++) {
+        fields[i].addEventListener('select', onafterselect, false);
+    }
+}
