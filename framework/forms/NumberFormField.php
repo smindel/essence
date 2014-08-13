261f806f62a89866ae54284937c9ad60a068edb6
Andreas Piening <piening@3online.de>
2014-08-13 12:43:38 +0200

    numberformfield extended by min, max, step

--- framework/forms/NumberFormField.php
+++ framework/forms/NumberFormField.php
@@ -1,3 +1,15 @@
 <?php
 
-class NumberFormField extends FormField {}
+class NumberFormField extends FormField
+{
+    protected $min;
+    protected $max;
+    protected $step;
+
+    public function getMin() { return $this->min; }
+    public function setMin($min) { $this->min = $min; return $this; }
+    public function getMax() { return $this->max; }
+    public function setMax($max) { $this->max = $max; return $this; }
+    public function getStep() { return $this->step; }
+    public function setStep($step) { $this->step = $step; return $this; }
+}
